5a45bc618cf8048a9141e910085e5c88153495b5
[JefferyLWY] <[jefferylau.jl@gmail.com]>
2019-01-06 12:06:40 +0800

    Updated codes and recompiled

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kinesis Spark Streaming template
--- src/main/scala/Kinesis/testStreaming.scala
+++ src/main/scala/Kinesis/testStreaming.scala
@@ -12,40 +12,50 @@ import org.apache.spark.streaming.kinesis.{KinesisInputDStream, KinesisInitialPo
 
 import org.apache.log4j.{Level, Logger}
 
-object testStreaming {
+object testStreaming extends Logging {
   def main(args: Array[String]): Unit = {
 
-    val appName = "KinesisStreaming"
+    //Kinesis Configurations
+    val appName = "FYPKinesisStreaming"
     val streamName = "logStream"
     val endpointUrl = "https://kinesis.us-east-1.amazonaws.com"
     val region = "us-east-1"
 
+    //AWS Configurations
     val endpoint = new EndpointConfiguration(endpointUrl, region)
     val credentials = new DefaultAWSCredentialsProviderChain()
     require(credentials != null,
       "No AWS credentials found. Please specify credentials using one of the methods specified")
 
-    //StreamingLogger.setStreamingLogLevels(Level.WARN)
+    //Log Configurations
+    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+    if (!log4jInitialized) {
+      logInfo("Setting log level to [WARN] for streaming application.")
+      Logger.getRootLogger.setLevel(Level.WARN)
+    }
 
+    //Configure Kinesis Client
     val builder = AmazonKinesisClient.builder()
     builder.setCredentials(credentials)
     builder.setEndpointConfiguration(endpoint)
-    println("Build successful...")
+    println("Build successful, Getting Stream Info:")
 
+    //Verify Stream
     val kinesisClient = builder.build()
     val streamInfo = kinesisClient.describeStream(streamName).getStreamDescription()
     val numShards = streamInfo.getShards.size()
-    println("Stream Info:\n" + streamInfo.toString)
+    println(streamInfo.toString)
 
-    val numStreams = numShards
-    val batchInterval = Seconds(2)
+    //Configure Spark
+    val batchInterval = Seconds(5)
     val kinesisCheckpointInterval = batchInterval
 
     val sparkConfig = new SparkConf().setAppName(appName).setMaster("local[2]")
     val sc = new SparkContext(sparkConfig)
     val ssc = new StreamingContext(sc, batchInterval)
 
-    val kinesisStreams = (0 until numStreams).map { i =>
+    //Build Kinesis Stream
+    val kinesisStreams = (0 until numShards).map { i =>
       KinesisInputDStream.builder
         .streamingContext(ssc)
         .streamName(streamName)
@@ -58,9 +68,13 @@ object testStreaming {
         .build()
     }
 
+    //Code
     val unionStreams = ssc.union(kinesisStreams)
-    unionStreams.print()
+    val logStream = unionStreams.flatMap(byteArray => new String(byteArray))
+    logStream.print()
+    //End of Code
 
+    println("All set, Spark starting...")
     ssc.start()
     ssc.awaitTermination()
 
@@ -68,12 +82,3 @@ object testStreaming {
 
 }
 
-//private[streaming] object StreamingLogger extends Logging {
-//  def setStreamingLogLevels(level: Level) {
-//    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
-//    if (!log4jInitialized) {
-//      logInfo("Setting log level to [WARN] for streaming application.")
-//      Logger.getRootLogger.setLevel(level)
-//    }
-//  }
-//}
